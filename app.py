d159973f0c5fea7d10ad7fb7d2c99d1f1cc4e085
charco <l22070008@huetamo.tecnm.mx>
2025-11-22 20:05:26 -0600

    creado la estructura inicial del proyecto

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/vocabulario")
+def vocabulario():
+    return render_template("bocabulario.html")
+
+@app.route("/juegos")
+def juegos():
+    return render_template("juegos.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/js/scrpipt.js
@@ -0,0 +1,1 @@
+console.log("Kids English cargado.");
