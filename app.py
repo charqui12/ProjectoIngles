fdcb779106e735bfd5654cce5a2943066a9e5865
charco <l22070008@huetamo.tecnm.mx>
2025-11-25 22:13:46 -0600

    agregando funcionalidad a los modulos de comidas, ropas y colores

--- app.py
+++ app.py
@@ -13,6 +13,15 @@ def vocabulario():
 @app.route("/animales")
 def animales():
     return render_template("animales.html")
+@app.route("/comidas")
+def comidas():
+    return render_template("comidas.html")
+@app.route("/ropa")
+def ropa():
+    return render_template("ropa.html")
+@app.route("/colores")
+def colores():
+    return render_template("colores.html")
 
 @app.route("/juegos")
 def juegos():
